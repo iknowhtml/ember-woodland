26135dfc68d1d2391f2270c345b05e22e34d48be
iknowhtml <iknowsmtp@gmail.com>
2017-06-19 15:09:02 -0400

    complete level 3. models and services, 3.1, 3.2, 3.3, 3.4

---
+++ app/services/store.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+import Option from 'woodland/models/option';
+import Poll from 'woodland/models/poll';
+
+const polls = [
+  Poll.create({
+    id: '1',
+    question: 'Which Poisonous Plant Are You?',
+    options: [
+      Option.create({ id: '1', value: 'Nightshade' }),
+      Option.create({ id: '2', value: 'Hemlock' }),
+      Option.create({ id: '3', value: 'Rhubarb' })
+    ]
+  }),
+
+  Poll.create({
+    id: '2',
+    question: 'Which Is Your Favorite Woodland Wanderer Way?',
+    options: [
+      Option.create({ id: '4', value: 'Honesty' }),
+      Option.create({ id: '5', value: 'Integrity' }),
+      Option.create({ id: '6', value: 'Patience' })
+    ]
+  })
+];
+
+export default Ember.Service.extend({
+  getPolls() {
+    return polls;
+  },
+  getPollById(id){
+    return polls.findBy('id', id);
+  }
+});
