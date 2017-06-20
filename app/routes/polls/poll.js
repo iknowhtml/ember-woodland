2eef066c6e678ff37db24f54decd16d9e2c76f57
iknowhtml <iknowsmtp@gmail.com>
2017-06-20 12:56:44 -0400

    complete 4.6, enable voting

--- app/routes/polls/poll.js
+++ app/routes/polls/poll.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    voteForOption(poll, option){
+      option.incrementProperty('votes');
+      this.transitionTo('polls.results', poll);
+    }
+  },
   model(params){
     return this.get('store').getPollById(params.poll_id);
   }
